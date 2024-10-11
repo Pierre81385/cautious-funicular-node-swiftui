e572c6a935f10d98e6c3427f1ed55d6955b6947d
Peter Bishop <pjb.den@gmail.com>
2024-10-11 01:38:33 -0600

    user image gallery working and looking good

---
+++ mobile/cautious-funicular/cautious-funicular/Authentication/ProfileImageGalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileImageGalleryView.swift
+//  cautious-funicular
+//
+//  Created by m1_air on 10/10/24.
+//
+
+import SwiftUI
+
+struct ProfileImageGalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileImageGalleryView()
+}
